fd3af45b1e39ddcd9baed20f5038ecf438c8d130
Audrius Dailydė <audrius.dailyde@gmail.com>
2020-01-12 20:35:14 +0200

    initial learnt things

---
+++ 2_tuples-strings-loops/tutorial2.py
@@ -0,0 +1,11 @@
+# Tuple
+programming_languages = "Python", "Java", "C++", "C#"
+print(type(programming_languages))
+
+# Tuple with parentheses
+programming_languages_with_parentheses = ("Python", "Java", "C++", "C#")
+print(type(programming_languages_with_parentheses))
+
+# Loop
+for language in programming_languages:
+    print(language)
---
+++ 4_index-slices/indexes.py
@@ -0,0 +1,12 @@
+l = [1,2,3,4,5]
+
+
+print(l[1])     # 2
+print(l[4])     # 5
+print(l[-1])    # 5
+print(l[1:3])   # slice [2,3]
+print(l[2:])    # slice [3,4,5]
+
+print(l)
+l[1] = 99
+print(l)
